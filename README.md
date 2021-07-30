4bba60905bf76ca4a596a592b1680da70b17acc1
Xaintailles <calixte.allier@gmail.com>
2021-07-30 17:35:33 +0200

    Starting to pull the 10 minutes files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # weather_data
  Tryign a new project to compare predictions with actuals
+ 
+ Documentation:
+ 
+ https://developer.dataplatform.knmi.nl/
+ https://developer.dataplatform.knmi.nl/example-scripts
--- api_pulls.py
+++ api_pulls.py
@@ -8,15 +8,28 @@ Created on Fri Jul 30 16:51:18 2021
 
 import requests
 import json
+from datetime import datetime
 
 with open("credentials.json") as json_file:
         json_credentials = json.load(json_file)
 
+root = 'https://api.dataplatform.knmi.nl/open-data'
+version_id = 2
+dataset_name = 'Actuele10mindataKNMIstations'
+extension = f'/v1/datasets/{dataset_name}/versions/{version_id}/files'
 
+url = str(root) + str(extension)
 
-headers = {"Authorization": json_credentials['OpenData']}
+headers = {"Authorization": json_credentials[dataset_name]}
 
-r = requests.get('https://api.dataplatform.knmi.nl/open-data/v1/datasets/Tn1/versions/2/files'
-                 , headers = headers)
+max_keys = str(10)
+
+timestamp = datetime.utcnow().date().strftime("%Y%m%d")
+start_after_filename_prefix = f"KMDS__OPER_P___10M_OBS_L2_{timestamp}"
+
+r = requests.get(url = url
+                 ,headers = headers
+                 ,params={"maxKeys": max_keys, "startAfterFilename": start_after_filename_prefix} 
+                 )
 
 test = r.json()
